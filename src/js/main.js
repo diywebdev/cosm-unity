08187f26ad43a834279de8c6399e61af1ba8e87e
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-10-21 14:42:33 +0500

    BANNER - start

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log('Working...');
+const $siteHeader = document.querySelector('.header');
+
+if(document.body.getBoundingClientRect().top !== 0) {
+    $siteHeader.classList.add('fixed');
+}
+
+document.addEventListener('scroll', () => {    
+    if (window.scrollY > 10) {
+        $siteHeader.classList.add('fixed');
+    } else {
+        $siteHeader.classList.remove('fixed');
+    }
+});
+
+// window.addEventListener('resize', () => {
+//     // this.open = false;
+//     // hideHeaderPos = $siteHeader.querySelector('header').clientHeight;
+// });
+
